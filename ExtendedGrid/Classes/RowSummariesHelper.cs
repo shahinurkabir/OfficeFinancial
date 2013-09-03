5fd1e1184a5b33efc75ad0c4e0c78c87d42d16b8
Shahinur Kabir <shahinurkabir@yahoo.com>
2013-09-03 23:46:11 +0600

    Revert "Project Hosted"
    This reverts commit 4fb5575289814fa254a3f8bba0d9b031d7176676.

--- ExtendedGrid/Classes/RowSummariesHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExtendedGrid.Classes
-{
-    class RowSummariesHelper
-    {
-      
-
-        public static string CurrentColumn { get; set; }
-    }
-}
--- OfficeFinancial.Common/ComboBoxItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OfficeFinancial.Common
-{
-    public class ComboBoxItem
-    {
-        public string Key { get; set; }
-
-        public string Value { get; set; }
-    }
-}
